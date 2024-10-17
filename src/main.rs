dfc563223db937ca5f21363cee8da0147b1be712
William D. Jones <thor0505@comcast.net>
2024-10-16 23:57:10 -0400

    Add hyperlinking. There are several problems, but the minimum works.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+use eframe::egui::text::LayoutJob;
+use eframe::egui::Link;
 use eframe::egui::Response;
+use eframe::egui::Ui;
 use env_logger;
 use log::debug;
+use oneshot::Receiver;
+use oneshot::Sender;
 use oneshot::{self};
 use url::{ParseError, Url};
 
 use std::cell::RefCell;
+use std::collections::HashMap;
 use std::io::Read;
 use std::io::Write;
 use std::net::TcpStream;
@@ -71,6 +77,23 @@ fn bg_thread(recv: mpsc::Receiver<(String, oneshot::Sender<Result<String>>)>) {
     }
 }
 
+fn start_new_url(
+    url: &Url,
+    cmd_send: &mpsc::Sender<(String, oneshot::Sender<Result<String>>)>,
+    recv_: &mut Receiver<Result<String>>,
+    link_present: &mut Vec<Option<Url>>,
+    state: &mut ControlFlow,
+) {
+    let (send, recv_tmp_) = oneshot::channel();
+    *recv_ = recv_tmp_;
+
+    let _ = cmd_send.send((url.to_string(), send));
+
+    link_present.clear();
+    *state = ControlFlow::Waiting;
+    return;
+}
+
 fn main() -> Result<(), eframe::Error> {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
 
@@ -89,6 +112,7 @@ fn main() -> Result<(), eframe::Error> {
     let mut url_string = String::from("nex://nex.nightfall.city/");
     let _ = cmd_send.send((url_string.clone(), send));
     let nex_string = RefCell::new(String::new());
+    let mut link_present: Vec<Option<Url>> = Vec::new();
 
     eframe::run_simple_native("swmon", options, move |ctx, _frame| {
         // Render top to bottom
@@ -101,6 +125,7 @@ fn main() -> Result<(), eframe::Error> {
                     recv_ = recv_tmp_;
 
                     let _ = cmd_send.send((url_string.clone(), send));
+                    link_present.clear();
                     state = ControlFlow::Waiting;
                 }
             });
@@ -115,6 +140,7 @@ fn main() -> Result<(), eframe::Error> {
                 match recv_.try_recv() {
                     Ok(Ok(recv)) => {
                         *nex_string.borrow_mut() = recv;
+                        link_present.clear();
                         state = ControlFlow::Idle;
                     }
                     _ => {}
@@ -124,7 +150,62 @@ fn main() -> Result<(), eframe::Error> {
                 egui::ScrollArea::vertical()
                     .auto_shrink([false, false])
                     .show(ui, |ui| {
-                        ui.label(egui::RichText::new(&*nex_string.borrow()).monospace());
+                        for (i, line) in nex_string.borrow().lines().enumerate() {
+                            match link_present.get(i) {
+                                Some(Some(url)) if line.starts_with("=> ") => {
+                                    if ui.link(&line[3..line.len()]).clicked() {
+                                        start_new_url(&url.clone(), &cmd_send, &mut recv_, &mut link_present, &mut state);
+                                        return;
+                                    }
+                                    ui.label(egui::RichText::new("\n").monospace());
+                                }
+                                Some(Some(_)) => {
+                                    unreachable!()
+                                }
+                                Some(None) => {
+                                    ui.label(egui::RichText::new(line).monospace());
+                                }
+                                None if line.starts_with("=> ") => {
+                                    assert!(link_present.len() == i);
+                                    match Url::parse(&line[3..line.len()]) {
+                                        Ok(url) => {
+                                            link_present.push(Some(url.clone()));
+
+                                            if ui.link(&line[3..line.len()]).clicked() {
+                                                start_new_url(&url, &cmd_send, &mut recv_, &mut link_present, &mut state);
+                                                return;
+                                            }
+                                        }
+                                        Err(_) => {
+                                            let abs_url = match Url::parse(&url_string) {
+                                                Ok(url) => url.join(&line[3..line.len()]),
+                                                Err(_) => {
+                                                    link_present.push(None);
+                                                    ui.label(egui::RichText::new(line).monospace());
+                                                    continue;
+                                                }
+                                            };
+                                            debug!(target: "nex-ballast-fg", "url didn't parse... treating as relative {:?}", &abs_url);
+                                            match abs_url
+                                            {
+                                                Ok(url) => {
+                                                    link_present.push(Some(url.clone()));
+                                                }
+                                                Err(_) => {
+                                                    link_present.push(None);
+                                                    ui.label(egui::RichText::new(line).monospace());
+                                                    continue;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                                None => {
+                                    link_present.push(None);
+                                    ui.label(egui::RichText::new(format!("{}\n", line)).monospace());
+                                }
+                            }
+                        }
                     });
             }
         });
